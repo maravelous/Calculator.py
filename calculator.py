2db916e8bdb203f8467b14dd8099c720e7f7ab91
Mara <maravelous77@gmail.com>
2025-08-19 15:55:58 -0700

    Add basci calculator with +,-,*,/

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
-print("Hello, Calculator!")
+print("Welcome to the Python Calculator!")
+
+num1 = float(input("Enter the first number: "))
+op = input("Enter an operation (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op ++ "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    if num2 != 0:
+        print(num1 / num2)
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operation.")
